547804c5c79009780ef0efa5bce1d317fa8e0ba6
DyshliukAnna <113441385+DyshliukAnna@users.noreply.github.com>
2023-12-07 17:16:45 +0200

    Comment over classes in module.css

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,18 +1,23 @@
 import PropTypes from 'prop-types';
 import css from './FriendListItem.module.css';
 
-const FriendListItem = ({avatar, name, isOnline}) => {
-    return <li className={css.item}>
-        <span className={css.status} style={{ backgroundColor: isOnline ? 'rgb(10, 55, 50)' : 'red' }}></span>
-            <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
-            <p className={css.name}>{name}</p>
-        </li>;
-}
+const FriendListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <li className={css.item}>
+      <span
+        className={css.status}
+        style={{ backgroundColor: isOnline ? 'rgb(7, 174, 21)' : 'red' }}
+      ></span>
+      <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+      <p className={css.name}>{name}</p>
+    </li>
+  );
+};
 
 export default FriendListItem;
 
 FriendListItem.propTypes = {
-    avatar: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    isOnline: PropTypes.bool.isRequired
-}
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+};
